4232a90c59cf7a941368bf34893c83082fe047a8
Max Stripp <maxbstripp@gmail.com>
2014-09-30 13:32:36 +0200

    Thank You Bjarne!!! 1B done thank god

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -10,38 +10,25 @@ namespace httpserver
 {
     public class HttpServer
     {
-       public static readonly int DefaultPort = 6789;
+       
+
+       // vi laver en statisk port og navngiver den DefaultPort
+       public static readonly int DefaultPort = 8888;
+
 
        public void StartServer()
        {
            //creates a server socket/listner/welcome socket
            TcpListener serverSocket = new TcpListener(DefaultPort);
            serverSocket.Start();
-           Console.Write("server is started");
+
            //creates a connectionSocket by accepting the connection request from the client
            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-           Service service = new Service(connectionSocket);
-           service.SocketHandler();
+           Console.Write("Server is activated");
 
-       }
-            //network stream for the connected client; to read from or write to
-            Stream ns = new NetworkStream(connectionSocket);
-            StreamReader sr = new StreamReader(ns);
-
-
-
-            StreamWriter sw = new StreamWriter(ns);
-            sw.WriteLine("Hello browser!");
-            sw.AutoFlush = true;
-
-            //saves the lines read fromteh stream in a string variable and print it on the scren
-            string message = sr.ReadLine();
-            Console.WriteLine("write to console worked!");
-
-
-            ns.Close();
-            connectionSocket.Close();
-            serverSocket.Stop();
+           Service ser = new Service(connectionSocket);
+           ser.SocketService();
+           serverSocket.Stop();
         }
     }
     }
--- httpserver/Service.cs
+++ httpserver/Service.cs
@@ -8,26 +8,44 @@ using System.Threading.Tasks;
 
 namespace httpserver
 {
-    class Service
+    internal class Service
     {
         private TcpClient connectionSocket;
+
         public Service(TcpClient connectionSocket)
         {
             this.connectionSocket = connectionSocket;
         }
-        
 
-       public void SocketHandler()
+        public void SocketService()
         {
+
+            //network stream for the connected client; to read from or write to
             Stream ns = connectionSocket.GetStream();
-          //  StreamReader sr = new StreamReader(ns);
+            StreamReader sr = new StreamReader(ns);
             StreamWriter sw = new StreamWriter(ns);
             sw.AutoFlush = true;
-           
-            sw.Write("<html><body>Hello Chrome</body></html>");
-            //sw.WriteLine("Printing many lines");
-            connectionSocket.Close();
 
+            sw.WriteLine("Hello browser!");
+            sw.WriteLine("Hello many times");
+            sw.WriteLine(
+                "<HTML><HEAD><TITLE>My Home Page</TITLE></HEAD><BODY><H1>My Home Page</H1>Hi There!</BODY></HTML>");
+
+
+
+            // message det vi skriver i browseren i dette tilfælde localhost:8888/arg --> sr.Readline læser alle request.
+            string message = sr.ReadLine();
+            //adskil opdeler message ved hjælp af indbygget Split metode.
+            string[] adskil = message.Split(' ');
+            // sw.writeline udskriver element '1'  i browseren i dette tilfælde /arg
+            sw.WriteLine(adskil[1]);
+
+
+
+
+
+            ns.Close();
+            connectionSocket.Close();
         }
     }
 }
