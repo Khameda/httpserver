8b3999d5b6266c7cda56b7ddf007d5603c803be0
Max Stripp <maxbstripp@gmail.com>
2014-10-01 14:56:47 +0200

    recover commit

---
+++ httpserver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+namespace httpserver
+{
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            //
+           
+            // Vi laver et nyt objekt af klassen Httpserver (server)   
+            HttpServer server = new HttpServer();
+            // Vi kalder metoden Startserver via vores objekt server
+            server.StartServer();
+
+            Console.Read();
+
+            
+        }
+    }
+}
---
+++ httpserver/Service.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpserver
+{
+    internal class Service
+    {
+
+        /// Vi declare vores RootCatalog med placering "c:/temp"  
+        /// Tanken er hvis vi har brug for flere filer opretter vi en seperat klasse dedikeret til filer eks. Filkatalog
+        private static readonly string RootCatalog = "c:/temp";
+
+        private TcpClient connectionSocket;
+
+        public Service(TcpClient connectionSocket)
+        {
+            this.connectionSocket = connectionSocket;  
+        }
+        public void SocketService()
+        {
+              
+            //network stream for the connected client; to read from or write to
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            sw.Write("HTTP/1.0 200 OK\r\n");
+            sw.Write("\r\n");
+            sw.Write("Hello world");
+
+
+           
+          
+
+                //SendREFile(RootCatalog, sw);
+                //GetREfilePath();
+
+
+         
+
+                // message det vi skriver i browseren i dette tilfælde localhost:8888/arg --> sr.Readline læser alle request.
+                //adskil opdeler message ved hjælp af indbygget Split metode.
+                // sw.writeline udskriver element '1'  i browseren i dette tilfælde /arg
+
+            try
+            {
+                string message = sr.ReadLine();
+                string[] adskil = message.Split(' ');
+                sw.WriteLine(adskil[1]);
+
+                string temppath = RootCatalog + adskil[1];
+
+                if (File.Exists(temppath))
+                {
+                    using (FileStream fs = File.OpenRead(temppath))
+                    {
+
+
+                        byte[] b = new byte[1024];
+                        UTF8Encoding temp = new UTF8Encoding(true);
+                        while (fs.Read(b, 0, b.Length) > 0)
+                        {
+                            sw.WriteLine(temp.GetString(b));
+                        }
+                    }
+                }
+            }
+            catch
+                (Exception BadMessage)
+            {
+                Console.WriteLine(BadMessage);
+            }
+
+
+
+
+
+
+
+            ns.Close();
+            connectionSocket.Close();
+        }
+
+
+        //private string GetREfilePath(string message)
+        //{
+        //    string[] adskil = message.Split(' ');
+            
+        //    string result = adskil[1];
+
+
+        //    return result;
+        //}
+
+      
+
+        //private void SendREFile(string Rootcatalog, StreamWriter sw)
+        //{
+        //    using (FileStream fs = File.OpenRead(Rootcatalog))
+        //    {
+        //        byte[] b = new byte[1024];
+        //        UTF8Encoding temp = new UTF8Encoding(true);
+        //        while (fs.Read(b, 0, b.Length) > 0)
+        //        {
+        //            sw.WriteLine(temp.GetString(b));
+        //        }
+        //    }
+        //}
+        
+    }
+}
